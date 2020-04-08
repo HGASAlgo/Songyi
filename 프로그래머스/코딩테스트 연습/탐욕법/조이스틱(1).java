4f52c59fb8f1bb62d0460f5cf5f21f528ffdba41
Songyizzang <33695932+Songyizzang@users.noreply.github.com>
2020-04-09 03:27:13 +0900

    조이스틱(1)
    정확성 100/100

--- 프로그래머스/코딩테스트 연습/탐욕법/조이스틱(1).java
+++ 프로그래머스/코딩테스트 연습/탐욕법/조이스틱(1).java
@@ -2,10 +2,10 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         
-        int exp = name.length() - 1;
+        int exp = name.length() - 1; // 처음부터 끝까지 셀 경우(최대값)
         for(int i = 0; i < name.length(); i++){
             char c = name.charAt(i);
-            answer += ('Z'- c + 1) > c - 'A' ? c - 'A' : ('Z' - c + 1);
+            answer += ('Z'- c + 1) > c - 'A' ? c - 'A' : ('Z' - c + 1); // A부터 세거나 Z부터(거꾸로)세서 더 작은 값 추출
             if(c == 'A'){
                 int nextIdx = i+1;
                 int countA = 0;
