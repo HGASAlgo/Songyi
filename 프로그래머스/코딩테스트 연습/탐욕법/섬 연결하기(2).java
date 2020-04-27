60dbe312c1e0f10cdeb06e9acbd8d6ca0449e312
Songyizzang <33695932+Songyizzang@users.noreply.github.com>
2020-04-28 02:55:47 +0900

    섬 연결하기(2)
    100/100

--- 프로그래머스/코딩테스트 연습/탐욕법/섬 연결하기(2).java
+++ 프로그래머스/코딩테스트 연습/탐욕법/섬 연결하기(2).java
@@ -13,7 +13,7 @@ class Solution {
         visit[costs[0][1]] = true;	//	최소 비용의 다리를 건설해주기 위해 섬에 방문
         bridge[0] = true;	//	최소 비용의 다리를 건설함
         int answer = costs[0][2];	// 처음 건설한 다리의 비용 추가
-		int island = 2;	// 방문한 섬의 개수를 체크해 줄 변수
+	int island = 2;	// 방문한 섬의 개수를 체크해 줄 변수
         
         while(island < n) {	//	모든 섬을 방문하면 다리를 건설할 필요가 없음(=모든 섬을 방문)
         	for(int i = 1; i < costs.length; i++) {
