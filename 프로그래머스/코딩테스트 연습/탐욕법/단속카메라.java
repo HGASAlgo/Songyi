668a57138467f9958c09c10d7fdf6ac09786f6fb
Songyizzang <33695932+Songyizzang@users.noreply.github.com>
2020-04-30 21:49:22 +0900

    단속카메라
    100/100

--- 프로그래머스/코딩테스트 연습/탐욕법/단속카메라.java
+++ 프로그래머스/코딩테스트 연습/탐욕법/단속카메라.java
@@ -1,20 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-      int answer = 0;	//	카메라 개수
-		  int camera = -30001;	// 카메라 설치된 위치
+	int answer = 0;	//	카메라 개수
+	int camera = -30001;	// 카메라 설치된 위치
         
-      //	차량의 진출 지점을 기준으로 오름차순 정렬, 먼저 나간 차량부터 체크 하면 뒤에 걸리지 않은 차량들만 체크하면 됨
-		  Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
+      	// 차량의 진출 지점을 기준으로 오름차순 정렬, 먼저 나간 차량부터 체크 하면 뒤에 걸리지 않은 차량들만 체크하면 됨
+	Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
         
-      //	카메라가 설치된 위치가 진입 지점보다 전에 있으면 진출 지점에 카메라를 설치
-      //	진입 지점 보다 나중에 있으면 어차피 그 전에 설치한 카메라를 지날 수 밖에 없음
-      for(int[] route: routes) {
-        if(camera < route[0]) {
-          camera = route[1];
-          answer++;
-        }
-       }
-      return answer;
+      	//  카메라가 설치된 위치가 진입 지점보다 전에 있으면 진출 지점에 카메라를 설치
+      	//  진입 지점 보다 나중에 있으면 어차피 그 전에 설치한 카메라를 지날 수 밖에 없음
+      	for(int[] route: routes) {
+        	if(camera < route[0]) {
+			camera = route[1];
+          		answer++;
+        	}
+	}
+	return answer;
    }
 }
